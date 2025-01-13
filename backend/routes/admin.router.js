576343fc57aaa459c812e0a3d4cbe00bf8e99202
henakasenanayaka <10899691@students.plymouth.ac.uk>
2025-01-13 08:31:17 +0530

    admin panel and chat module done

--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { adminGetUsers, loadAdminPage } from "../controllers/admin.controller.js";
+import { adminChatPage, adminGetUsers, getChats, loadAdminPage } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 
 router.get("/index", loadAdminPage);
 router.get("/users", adminGetUsers);
+router.get("/chat", getChats);
+router.get("/:id", adminChatPage);
 
 export default router;
