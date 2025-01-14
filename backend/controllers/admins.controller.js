700e2c4c2aa09801527b8566c6a116914a657053
Prabhath-Nuge <prabhathnugekotuwa@gmail.com>
2025-01-14 11:05:16 +0530

    Bugs were fixed

--- backend/controllers/admins.controller.js
+++ backend/controllers/admins.controller.js
@@ -1,3 +1,4 @@
+import Customer from "../models/customer.model.js";
 import Room from "../models/room.model.js";
 
 export const adminGetAllRooms = async (req,res)=>{
@@ -95,3 +96,126 @@ export const adminAddRoom = async (req,res)=>{
     }
 
 };
+
+export const adminGetAdmins = async (req,res)=>{
+    if (!req.session.cus) {
+        return res.render("index.ejs", { success: false, message: "You must log in first" });
+    }
+    if (req.session.cus.type == "admin") {
+        const customers = await Customer.find({ type: "admin" });
+        res.render("admingetadmins.ejs", { data: customers });
+    } else {
+        return res.render("index.ejs", { success: false, message: "You must log in first" });
+    }
+}
+
+export const adminGetAdminEditPage = async (req,res)=>{
+    if (!req.session.cus || !req.session.cus._id) {
+        return res.status(400).render("index.ejs", { success: false, message: "Please log in first!" });
+    }
+    else if (!(req.session.cus.type == "admin")) {
+        return res.status(400).render("index.ejs", { success: false, message: "You are not an Admin" });
+    }
+
+    const id = req.params.id;
+    if(!id){
+        return res.status(400).render("adminIndex.ejs",{success:false,message:"Unauthorized!"});
+    }
+
+    const admin = await Customer.findById(id);
+    res.status(200).render("admineditadmin.ejs",{data:admin});
+}
+
+export const adminEditAdmin = async (req,res)=>{
+    if (!req.session.cus || !req.session.cus._id) {
+        return res.status(400).render("index.ejs", { success: false, message: "Please log in first!" });
+    }
+    else if (!(req.session.cus.type == "admin")) {
+        return res.status(400).render("index.ejs", { success: false, message: "You are not an Admin" });
+    }
+
+    try {
+        const {id,name,email,pnumber,address,dob,password,} = req.body;
+        if(!id||!name||!email||!pnumber||!address||!dob||!password){
+            return res.status(400).render("index.ejs",{success:false,message:"Must fill all required fields!"});
+        }
+    
+        const newCus = await Customer.findByIdAndUpdate(id,{
+            name,email,pnumber,address,dob,password
+        },{new:true});
+    
+        if(newCus){
+            req.session.cus = newCus;
+            return res.status(200).render("index.ejs",{success:true,message:"Updated Successfully"});
+            
+        }else{
+            return res.status(400).render("index.ejs",{success:true,message:"Something Went Wrong!"});
+        }
+    } catch (error) {
+        return res.status(500).render("index.ejs",{success:false,message:"Server Error:"+error});
+    }
+}
+
+export const adminDeleteAdmin = async (req,res)=>{
+    if (!req.session.cus || !req.session.cus._id) {
+        return res.status(400).render("index.ejs", { success: false, message: "Please log in first!" });
+    }
+    else if (!(req.session.cus.type == "admin")) {
+        return res.status(400).render("index.ejs", { success: false, message: "You are not an Admin" });
+    }
+
+    const id = req.params.id;
+    const deleteUser = await Customer.findByIdAndDelete(id);
+    res.status(200).render("adminIndex.ejs",{success:true,message:"Deleted Successfully"});
+}
+
+export const adminGetAddAdminPage = async (req,res)=>{
+    if (!req.session.cus || !req.session.cus._id) {
+        return res.status(400).render("index.ejs", { success: false, message: "Please log in first!" });
+    }
+    else if (!(req.session.cus.type == "admin")) {
+        return res.status(400).render("index.ejs", { success: false, message: "You are not an Admin" });
+    }
+
+    res.status(200).render("adminaddadmin.ejs");
+    
+}
+
+export const adminAddAdmin = async (req,res)=>{
+    const { name, email, pnumber, address, dob, password } = req.body;
+
+    if (!req.session.cus || !req.session.cus._id) {
+        return res.status(400).render("index.ejs", { success: false, message: "Please log in first!" });
+    }
+    else if (!(req.session.cus.type == "admin")) {
+        return res.status(400).render("index.ejs", { success: false, message: "You are not an Admin" });
+    }
+
+    if (!name || !email || !pnumber || !address || !dob || !password) {
+        console.log("All fields are required.");
+        return res.status(400).send("All fields are required.");
+    }
+
+    try {
+        const existingUser = await Customer.findOne({ email });
+        if (existingUser) {
+            console.log("User already exists with this email.");
+            return res.status(400).send("User already exists.");
+        }
+        const newUser = new Customer({
+            name,
+            email,
+            pnumber,
+            address,
+            dob,
+            password,
+            type: "admin"
+        });
+        await newUser.save();
+        console.log("User registered successfully:", newUser);
+        res.render("index", { success: true, message: "Registered Successfully! Now you can Login." });
+    } catch (error) {
+        console.error("Error during registration:", error);
+        res.status(500).send("An error occurred. Please try again later.");
+    }
+}
--- backend/routes/admins.router.js
+++ backend/routes/admins.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminAddRoom, adminAddRoomPage, adminGetAllRooms, adminRoomDelete, adminRoomEdit, adminRoomEditPage } from "../controllers/admins.controller.js";
+import { adminAddAdmin, adminAddRoom, adminAddRoomPage, adminDeleteAdmin, adminEditAdmin, adminGetAddAdminPage, adminGetAdminEditPage, adminGetAdmins, adminGetAllRooms, adminRoomDelete, adminRoomEdit, adminRoomEditPage } from "../controllers/admins.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +9,12 @@ router.post("/rooms/add", adminAddRoom);
 router.post("/rooms/edit", adminRoomEdit);
 router.get("/rooms/edit/:id", adminRoomEditPage);
 router.get("/rooms/delete/:id", adminRoomDelete);
+router.get("/admins",adminGetAdmins);
+router.get("/edit/:id", adminGetAdminEditPage);
+router.post("/admin/save", adminEditAdmin);
+router.get("/admin/delete/:id", adminDeleteAdmin);
+router.get("/addadmin", adminGetAddAdminPage);
+router.post("/addadmin", adminAddAdmin);
+
 
 export default router;
